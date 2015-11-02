dabde356cfe785aa31a8cf2fcd759b38fce8167a
Michel <copernicus_13@hotmail.com>
2015-11-02 22:04:11 +0100

    [2015-03] Écriture Exo 1 – Années bissextiles

--- 2015-03/Exo 1.cs
+++ 2015-03/Exo 1.cs
@@ -1,6 +1,22 @@
-﻿namespace BattleDev._2015_03
+﻿using System;
+
+namespace BattleDev._2015_03
 {
     public class Exo1
     {
+        public Exo1()
+        {
+            int nbLine = int.Parse(Console.ReadLine());
+
+            string line;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                int year = int.Parse(line);
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                    Console.WriteLine("BISSEXTILE");
+                else
+                    Console.WriteLine("NON BISSEXTILE");
+            }
+        }
     }
 }
