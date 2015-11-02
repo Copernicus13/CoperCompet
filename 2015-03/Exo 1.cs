5c2eb501f002d26d069f5e10bea1e3dd77988467
Michel <copernicus_13@hotmail.com>
2015-11-02 22:16:27 +0100

    [2015-03] Écriture Exo 2 – Format de mot de passe

--- 2015-03/Exo 1.cs
+++ 2015-03/Exo 1.cs
@@ -8,9 +8,9 @@ namespace BattleDev._2015_03
         {
             int nbLine = int.Parse(Console.ReadLine());
 
-            string line;
-            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            for (int i = 0; i < nbLine; ++i)
             {
+                string line = Console.ReadLine();
                 int year = int.Parse(line);
                 if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
                     Console.WriteLine("BISSEXTILE");
--- 2015-03/Exo 2.cs
+++ 2015-03/Exo 2.cs
@@ -1,6 +1,30 @@
-﻿namespace BattleDev._2015_03
+﻿using System;
+
+namespace BattleDev._2015_03
 {
     public class Exo2
     {
+        public Exo2()
+        {
+            int nbValid = 0;
+            int nbLine = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nbLine; ++i)
+            {
+                string line = Console.ReadLine();
+
+                if (line.Length == 6)
+                {
+                    if (char.IsLetter(line[0]) &&
+                        char.IsDigit(line[1]) &&
+                        char.IsLetter(line[2]) &&
+                        char.IsLetter(line[3]) &&
+                        char.IsLetter(line[4]) &&
+                        char.IsLetter(line[5]))
+                        ++nbValid;
+                }
+            }
+            Console.WriteLine(nbValid);
+        }
     }
 }
