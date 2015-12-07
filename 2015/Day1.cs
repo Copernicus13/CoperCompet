f4895e05e0c6e7be85b61ff78709ee43b651d992
Michel <copernicus_13@hotmail.com>
2015-12-07 14:09:59 +0100

    [2015] Initial commit with Day1 implementation.

---
+++ 2015/Day1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AdventOfCode.Common;
+
+namespace AdventOfCode._2015
+{
+    public class Day1
+    {
+        public Day1(Part p)
+        {
+            int cpt = 0, result = 0;
+            string line = Console.ReadLine();
+            foreach (var c in line)
+            {
+                ++cpt;
+                result += (c == '(' ? 1 : -1);
+                if (p == Part.Part2 && result == -1)
+                    break;
+            }
+            Console.WriteLine((p == Part.Part2 ? cpt : result));
+        }
+    }
+}
---
+++ Launcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Launcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new _2015.Day1(Part.Part2);
+            Console.Write("Appuyez sur une touche pour continuer…");
+            Console.ReadKey();
+        }
+    }
+}
