f3f8770e20e5e8ffd8e24679a3c54803a36af478
Michel <copernicus_13@hotmail.com>
2015-12-08 17:15:02 +0100

    [2015] Completed Day4

---
+++ 2015/Day4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using AdventOfCode.Common;
+
+namespace AdventOfCode._2015
+{
+    public class Day4
+    {
+        public Day4(Part p)
+        {
+            string line = Console.ReadLine();
+            long result;
+            string startWith = "00000";
+            if (p == Part.Part2)
+                startWith += "0";
+
+            for (result = 1; ; ++result)
+                using (var md5 = MD5.Create())
+                {
+                    if (BitConverter.ToString(
+                            md5.ComputeHash(Encoding.ASCII.GetBytes(string.Format("{0}{1}", line, result))))
+                            .Replace("-", string.Empty).StartsWith(startWith))
+                        break;
+                }
+            Console.WriteLine(result);
+        }
+    }
+}
