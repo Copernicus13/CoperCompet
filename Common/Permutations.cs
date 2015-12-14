a4a2d72e9359a1ed21883e2242ce995106718512
Michel <copernicus_13@hotmail.com>
2015-12-14 14:55:23 +0100

    [2015] Completed Day9

---
+++ Common/Permutations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AdventOfCode.Common
+{
+    public static class Permutations
+    {
+        public static bool GetNext(int[] perm)
+        {
+            int n = perm.Length;
+            int k = -1;
+
+            for (int i = 1; i < n; ++i)
+                if (perm[i - 1] < perm[i])
+                    k = i - 1;
+
+            if (k == -1)
+            {
+                Array.Reverse(perm);
+                return false;
+            }
+
+            int l = k + 1;
+            for (int i = l; i < n; ++i)
+                if (perm[k] < perm[i])
+                    l = i;
+
+            Utils.Swap(ref perm[k], ref perm[l]);
+
+            Array.Reverse(perm, k + 1, perm.Length - (k + 1));
+            return true;
+        }
+    }
+}
---
+++ Common/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode.Common
+{
+    public static class Utils
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
