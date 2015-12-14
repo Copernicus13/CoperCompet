59c70aa75ad624d485e97b11c3e9603ed3ee6a28
Michel <copernicus_13@hotmail.com>
2015-12-14 17:28:52 +0100

    [Common] Make Permutations.GetNext() method more generic.

--- Common/Permutations.cs
+++ Common/Permutations.cs
@@ -4,13 +4,13 @@ namespace AdventOfCode.Common
 {
     public static class Permutations
     {
-        public static bool GetNext(int[] perm)
+        public static bool GetNext<T>(T[] perm) where T : IComparable
         {
             int n = perm.Length;
             int k = -1;
 
             for (int i = 1; i < n; ++i)
-                if (perm[i - 1] < perm[i])
+                if (perm[i - 1].CompareTo(perm[i]) < 0)
                     k = i - 1;
 
             if (k == -1)
@@ -21,13 +21,21 @@ namespace AdventOfCode.Common
 
             int l = k + 1;
             for (int i = l; i < n; ++i)
-                if (perm[k] < perm[i])
+                if (perm[k].CompareTo(perm[i]) < 0)
                     l = i;
 
             Utils.Swap(ref perm[k], ref perm[l]);
 
             Array.Reverse(perm, k + 1, perm.Length - (k + 1));
             return true;
+        }
+
+        public static bool GetNext(ref string perm)
+        {
+            char[] charArray = perm.ToCharArray();
+            var result = GetNext(charArray);
+            perm = new string(charArray);
+            return result;
         }
     }
 }
