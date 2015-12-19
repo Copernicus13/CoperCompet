3b8af9017cfa923e9aa27bf42a899639bc38360f
Michel <copernicus_13@hotmail.com>
2015-12-19 18:08:43 +0100

    [2015] Completed Day17

---
+++ 2015/Day17.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode._2015
+{
+    public class Day17
+    {
+        public Day17(Part p)
+        {
+            IList<int> list = new List<int>();
+            string line;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+                list.Add(int.Parse(line));
+
+            var powerSet = Combinations.FastPowerSet(list.ToArray());
+
+            int result = 0;
+            if (p == Part.Part1)
+                powerSet.ToList()
+                    .ForEach(f => { if (f.Aggregate(0, (a, b) => a + b, c => c == 150)) ++result; });
+            else if (p == Part.Part2)
+            {
+                int minCont = int.MaxValue;
+                powerSet.ToList()
+                    .OrderBy(o => o.Length)
+                    .ToList()
+                    .ForEach(f =>
+                        {
+                            if (f.Aggregate(0, (a, b) => a + b, c => c == 150) && f.Length <= minCont)
+                            {
+                                minCont = f.Length;
+                                ++result;
+                            }
+                        });
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
