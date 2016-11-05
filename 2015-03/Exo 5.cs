dbb248a48a4585a13a4bbd865426fa8a2c799512
Michel <copernicus_13@hotmail.com>
2016-11-05 23:23:34 +0100

    [2015-03] Exo 5 – Notation polonaise

--- 2015-03/Exo 5.cs
+++ 2015-03/Exo 5.cs
@@ -1,6 +1,37 @@
-﻿namespace BattleDev._2015_03
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BattleDev._2015_03
 {
     public class Exo5
     {
+        public Exo5()
+        {
+            var stack = new Stack<int>();
+
+            var expression = Console.ReadLine();
+
+            foreach (string c in expression.Split(' ').Reverse())
+            {
+                int n;
+                if (!int.TryParse(c, out n))
+                {
+                    int x = stack.Pop();
+                    int y = stack.Pop();
+                    if (c == "+")
+                        n = x + y;
+                    else if (c == "-")
+                        n = x - y;
+                    else if (c == "*")
+                        n = x * y;
+                    else if (c == "/")
+                        n = Convert.ToInt32(Math.Floor(Convert.ToDecimal(x) / y));
+                }
+                stack.Push(n);
+            }
+
+            Console.WriteLine(stack.Peek());
+        }
     }
 }
