e40004712e80283568963036839f2bbd7ab033f2
Michel <copernicus_13@hotmail.com>
2016-11-21 21:55:53 +0100

    [2015] Completed Day23

---
+++ 2015/Day23.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode.Common;
+
+namespace AdventOfCode._2015
+{
+    public class Day23
+    {
+        private struct Processor
+        {
+            public int regA;
+            public int regB;
+
+            public int GetRegisterValue(string str)
+            {
+                return str.TrimEnd(',') == "a" ? regA : regB;
+            }
+
+            public void SetRegisterValue(string str, int value)
+            {
+                if (str.TrimEnd(',') == "a")
+                    regA = value;
+                else
+                    regB = value;
+            }
+        }
+
+        public Day23(Part p)
+        {
+            var proc = new Processor();
+            if (p == Part.Part2)
+                proc.regA = 1;
+
+            IList<string> instr = new List<string>();
+            for (string line; !string.IsNullOrEmpty(line = Console.ReadLine()); instr.Add(line))
+                ;
+            int ptr = 0;
+            while (ptr < instr.Count)
+            {
+                var words = instr[ptr].Split(' ');
+                var op = words[0].ToUpper();
+                switch (op)
+                {
+                    case "HLF":
+                        proc.SetRegisterValue(words[1], proc.GetRegisterValue(words[1]) >> 1);
+                        break;
+                    case "TPL":
+                        proc.SetRegisterValue(words[1], proc.GetRegisterValue(words[1]) * 3);
+                        break;
+                    case "INC":
+                        proc.SetRegisterValue(words[1], proc.GetRegisterValue(words[1]) + 1);
+                        break;
+                    case "JMP":
+                        ptr += short.Parse(words[1]) - 1;
+                        break;
+                    case "JIE":
+                        if (proc.GetRegisterValue(words[1]) % 2 == 0)
+                            ptr += short.Parse(words[2]) - 1;
+                        break;
+                    case "JIO":
+                        if (proc.GetRegisterValue(words[1]) == 1)
+                            ptr += short.Parse(words[2]) - 1;
+                        break;
+                }
+                ++ptr;
+            }
+            Console.WriteLine(proc.regB);
+        }
+    }
+}
