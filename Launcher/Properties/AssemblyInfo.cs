1305eab28177633f3ed4304bd9343414b34e5a20
Michel <copernicus_13@hotmail.com>
2016-11-25 21:39:05 +0100

    [Launcher] Update AssemblyInfo.cs

--- Launcher/Properties/AssemblyInfo.cs
+++ Launcher/Properties/AssemblyInfo.cs
@@ -1,25 +1,24 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("Launcher")]
 [assembly: AssemblyDescription("AdventOfCode launcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AdventOfCode")]
-[assembly: AssemblyCopyright("Copyright © Michel Pellegrin 2015")]
+[assembly: AssemblyCopyright("Copyright © Michel Pellegrin 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// L’affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+// COM, affectez la valeur true à l’attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
-// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+// Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("df6d5d18-07bc-477a-b1b1-b9c3ec58dba1")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
