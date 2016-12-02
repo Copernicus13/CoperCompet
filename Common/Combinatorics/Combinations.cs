278a4140b9e97c652b275c8184817874b37aace9
Michel <copernicus_13@hotmail.com>
2016-12-02 22:01:57 +0100

    [Common] Remove unused method.

--- Common/Combinatorics/Combinations.cs
+++ Common/Combinatorics/Combinations.cs
@@ -376,29 +376,6 @@ namespace AdventOfCode.Common.Combinatorics
                 .ToArray();
         }
 
-        public static T[][] FastGetAll(T[] seq, int rang)
-        {
-            var powerSet = new T[1 << seq.Length][];
-            powerSet[0] = new T[0]; // empty set
-            for (int i = 0; i < seq.Length; ++i)
-            {
-                var cur = seq[i];
-                int count = 1 << i;
-                for (int j = 0; j < count; ++j)
-                {
-                    var source = powerSet[j];
-                    if (source != null && source.Length < rang)
-                    {
-                        var destination = powerSet[count + j] = new T[source.Length + 1];
-                        for (int q = 0; q < source.Length; ++q)
-                            destination[q] = source[q];
-                        destination[source.Length] = cur;
-                    }
-                }
-            }
-            return powerSet;
-        }
-
         #endregion
     }
 }
