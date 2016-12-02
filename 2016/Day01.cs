1a9f4340b4af35a62b10962e7050d2c2b63306ed
Michel <copernicus_13@hotmail.com>
2016-12-02 22:02:40 +0100

    [2016] Completed Day01

--- 2016/Day01.cs
+++ 2016/Day01.cs
@@ -5,9 +5,43 @@ namespace AdventOfCode._2016
 {
     public class Day01
     {
+        private const int MAX_TAB_SIZE = 1000;
+        // North, West, South, East
+        private readonly int[] DirX = { 0, -1, 0, 1 };
+        private readonly int[] DirY = { 1, 0, -1, 0 };
+
         public Day01(Part p)
         {
-            throw new NotImplementedException();
+            char[,] tab = new char[MAX_TAB_SIZE, MAX_TAB_SIZE];
+            string line = Console.ReadLine();
+            int posX = MAX_TAB_SIZE / 2, posY = MAX_TAB_SIZE / 2;
+            tab[posX, posY] = '¤';
+            int actualDir = 0;
+            foreach (var seq in line.Split(','))
+            {
+                var instr = seq.Trim();
+                var lg = int.Parse(instr.Substring(1));
+                actualDir = (instr[0] == 'L' ? ++actualDir % 4 : --actualDir & 3);
+                if (p == Part.Part2)
+                {
+                    for (int i = 1; i <= lg; ++i)
+                    {
+                        if (tab[posX + i * DirX[actualDir],
+                                posY + i * DirY[actualDir]] == '¤')
+                        {
+                            Console.WriteLine(
+                                Math.Abs(MAX_TAB_SIZE / 2 - (posX + i * DirX[actualDir])) +
+                                Math.Abs(MAX_TAB_SIZE / 2 - (posY + i * DirY[actualDir])));
+                            return;
+                        }
+                        tab[posX + i * DirX[actualDir], posY + i * DirY[actualDir]] = '¤';
+                    }
+                }
+                posX += DirX[actualDir] * lg;
+                posY += DirY[actualDir] * lg;
+            }
+            Console.WriteLine(
+                Math.Abs(MAX_TAB_SIZE / 2 - posX) + Math.Abs(MAX_TAB_SIZE / 2 - posY));
         }
     }
 }
--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode.Launcher
         public static void Main(string[] args)
         {
             // ReSharper disable once ObjectCreationAsStatement
-            new _2015.Day25(Part.Part1);
+            new _2016.Day01(Part.Part2);
             Console.Write("Appuyez sur une touche pour continuer…");
             Console.ReadKey();
         }
