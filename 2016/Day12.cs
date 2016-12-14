bcc4d728e1a128738a3028cb2d46911a1bf85176
Michel P <mpellegrin@sodifrance.fr>
2016-12-14 13:39:00 +0100

    [2016] Completed Day12

--- 2016/Day12.cs
+++ 2016/Day12.cs
@@ -1,13 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Common;
 
 namespace AdventOfCode._2016
 {
     public class Day12
     {
+        private readonly IDictionary<string, int> dict = new Dictionary<string, int>();
+
         public Day12(Part p)
         {
-            throw new NotImplementedException();
+            IList<string> instr = new List<string>();
+            for (string line; !string.IsNullOrEmpty(line = Console.ReadLine()); instr.Add(line))
+                ;
+
+            if (p == Part.Part2)
+                dict["c"] = 1;
+            for (int i = 0; i < instr.Count; ++i)
+            {
+                var words = instr[i].Split(' ');
+                try
+                {
+                    switch (words[0])
+                    {
+                        case "cpy":
+                            dict[words[2]] = getOperand(words[1]);
+                            break;
+                        case "inc":
+                            ++dict[words[1]];
+                            break;
+                        case "dec":
+                            --dict[words[1]];
+                            break;
+                        case "jnz":
+                            if (getOperand(words[1]) != 0)
+                                i += int.Parse(words[2]) - 1;
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                }
+            }
+            Console.WriteLine(Environment.NewLine + dict["a"]);
+        }
+
+        private int getOperand(string v)
+        {
+            return isDigit(v) ? int.Parse(v) : dict[v];
+        }
+
+        private static bool isDigit(string v)
+        {
+            return v.All(c => char.IsDigit(c));
         }
     }
 }
