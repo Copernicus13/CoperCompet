6b7495ebf0e37f8541701f99ed5aa63d20cca4fd
Michel <copernicus_13@hotmail.com>
2016-12-16 19:54:58 +0100

    [Common] Add a useful data container Point to avoid importing System.Drawing one's, and add an ArrayHelpers to switch easily between multidimensional arrays and List<>.

---
+++ Common/Data/Point.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.Common.Data
+{
+    public struct Point<T>
+    {
+        public T x;
+        public T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public struct Point // Derivation from Point<int> not allowed for struct
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
