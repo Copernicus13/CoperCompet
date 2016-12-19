1ace978230605dbbc70d5543785b60dc15b06882
Michel <copernicus_13@hotmail.com>
2016-12-19 19:04:22 +0100

    [2016] Completed Day19

--- 2016/Day19.cs
+++ 2016/Day19.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AdventOfCode.Common;
 
 namespace AdventOfCode._2016
@@ -7,7 +8,36 @@ namespace AdventOfCode._2016
     {
         public Day19(Part p)
         {
-            throw new NotImplementedException();
+            int input = int.Parse(Console.ReadLine());
+            var list = new LinkedList<int>();
+
+            for (int i = 0; i < input; ++i)
+                list.AddLast(i);
+
+            var thief = list.First;
+            LinkedListNode<int> stolen = null;
+
+            if (p == Part.Part1)
+                stolen = GetNext(thief);
+            else if (p == Part.Part2)
+                stolen = list.Find(list.Count / 2);
+
+            while (list.Count > 1)
+            {
+                var nextToDead = GetNext(stolen);
+                list.Remove(stolen);
+                thief = GetNext(thief);
+                if (p == Part.Part1)
+                    stolen = GetNext(thief);
+                else if (p == Part.Part2)
+                    stolen = list.Count % 2 == 0 ? GetNext(nextToDead) : nextToDead;
+            }
+            Console.WriteLine(list.First.Value + 1);
+        }
+
+        private LinkedListNode<int> GetNext(LinkedListNode<int> next)
+        {
+            return next != next.List.Last ? next.Next : next.List.First;
         }
     }
 }
