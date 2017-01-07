9f784e5c6516d7cfa963fd43ef7b6c413964ce78
Michel <copernicus_13@hotmail.com>
2017-01-07 12:48:14 +0100

    [Launcher] Project initialization.

---
+++ Launcher/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HackerCup.Launcher
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Appuyez sur une touche pour continuer…");
+            Console.Read();
+        }
+    }
+}
