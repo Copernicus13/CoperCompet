aed2b29269263b071eb24a9fa65b6975061dba5e
Michel P <mpellegrin@sodifrance.fr>
2017-01-11 13:04:29 +0100

    [2016] Completed Day23

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -8,7 +8,7 @@ namespace AdventOfCode.Launcher
         public static void Main(string[] args)
         {
             // ReSharper disable once ObjectCreationAsStatement
-            new _2016.Day22(Part.Part2);
+            new _2016.Day23(Part.Part2);
             Console.Write("Appuyez sur une touche pour continuer…");
             Console.Read();
         }
