5c946729cc02db1c1a6a2bbe7a715b4e18ecc8e8
Michel P <copernicus_13@hotmail.com>
2017-04-06 23:36:43 +0200

    [GCJ] Add 2016 round 1b

---
+++ GoogleCodeJam/GCJ#/2016/Round 1b/1. GettingTheDigits.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleCodeJam._2016.Round1b
+{
+    public class GettingTheDigits
+    {
+        private IList<string> lol = new List<string>
+            {
+                "ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"
+            };
+
+        private IDictionary<char, int> dict = new Dictionary<char, int>();
+
+        public GettingTheDigits()
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int nbCase = 1; nbCase <= T; ++nbCase)
+            {
+                int[] nbApp = new int[10];
+                string str = Console.ReadLine();
+                string res = string.Empty;
+
+                dict.Clear();
+                for (char c = 'A'; c <= 'Z'; ++c)
+                    dict.Add(c, 0);
+
+                foreach (char c in str)
+                    ++dict[c];
+
+                nbApp[0] = dict['Z'];
+                nbApp[2] = dict['W'];
+                nbApp[6] = dict['X'];
+                nbApp[8] = dict['G'];
+                RemoveLetters(dict, 0, nbApp[0]);
+                RemoveLetters(dict, 2, nbApp[2]);
+                RemoveLetters(dict, 6, nbApp[6]);
+                RemoveLetters(dict, 8, nbApp[8]);
+                nbApp[3] = dict['H'];
+                RemoveLetters(dict, 3, nbApp[3]);
+                nbApp[7] = dict['S'];
+                RemoveLetters(dict, 7, nbApp[7]);
+                nbApp[5] = dict['V'];
+                RemoveLetters(dict, 5, nbApp[5]);
+                nbApp[4] = dict['F'];
+                RemoveLetters(dict, 4, nbApp[4]);
+                nbApp[9] = dict['I'];
+                RemoveLetters(dict, 9, nbApp[9]);
+                nbApp[1] = dict['N'];
+                RemoveLetters(dict, 1, nbApp[1]);
+
+                for (char c = '0'; c <= '9'; ++c)
+                    res += string.Join(string.Empty, Enumerable.Repeat(c, nbApp[c - '0']));
+
+                Console.WriteLine(string.Format("Case #{0}: {1}", nbCase, res));
+            }
+        }
+
+        private void RemoveLetters(IDictionary<char, int> dict, int v, int w)
+        {
+            foreach (char c in lol[v])
+                dict[c] -= w;
+        }
+    }
+}
---
+++ GoogleCodeJam/GCJ#/2016/Round 1b/2. CloseMatch.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace GoogleCodeJam._2016.Round1b
+{
+    public class CloseMatch
+    {
+        public CloseMatch()
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int nbCase = 1; nbCase <= T; ++nbCase)
+            {
+                string str = Console.ReadLine();
+                string C2 = str.Split(' ')[0];
+                string J2 = str.Split(' ')[1];
+
+                char[] C = C2.ToCharArray();
+                char[] J = J2.ToCharArray();
+
+                for (int i = 0; i < C.Length; ++i)
+                {
+                    int a, b;
+                    if (C[i] == '?' && J[i] == '?')
+                    {
+                        if (i == 0 ||
+                            int.Parse(string.Join(string.Empty, C).Substring(0, i)) == int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                        {
+                            if (i != C.Length - 1 && int.TryParse(string.Join(string.Empty, C).Substring(i + 1, 1), out a) && int.TryParse(string.Join(string.Empty, J).Substring(i + 1, 1), out b) &&
+                                a < b)
+                            {
+                                C[i] = '1';
+                                J[i] = '0';
+                            }
+                            else if (i != C.Length - 1 && int.TryParse(string.Join(string.Empty, C).Substring(i + 1, 1), out a) && int.TryParse(string.Join(string.Empty, J).Substring(i + 1, 1), out b) &&
+                                a > b)
+                            {
+                                C[i] = '0';
+                                J[i] = '1';
+                            }
+                            else
+                            {
+                                C[i] = '0';
+                                J[i] = '0';
+                            }
+                        }
+                        else if (int.Parse(string.Join(string.Empty, C).Substring(0, i)) < int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                        {
+                            C[i] = '9';
+                            J[i] = '0';
+                        }
+                        else
+                        {
+                            C[i] = '0';
+                            J[i] = '9';
+                        }
+                    }
+                    else if (C[i] == '?')
+                    {
+                        if (i == 0 || int.Parse(string.Join(string.Empty, C).Substring(0, i)) == int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            C[i] = J[i];
+                        else if (int.Parse(string.Join(string.Empty, C).Substring(0, i)) < int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            C[i] = '9';
+                        else if (int.Parse(string.Join(string.Empty, C).Substring(0, i)) > int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            C[i] = '0';
+                    }
+                    else if (J[i] == '?')
+                    {
+                        if (i == 0 || int.Parse(string.Join(string.Empty, C).Substring(0, i)) == int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            J[i] = C[i];
+                        else if (int.Parse(string.Join(string.Empty, C).Substring(0, i)) < int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            J[i] = '9';
+                        else if (int.Parse(string.Join(string.Empty, C).Substring(0, i)) > int.Parse(string.Join(string.Empty, J).Substring(0, i)))
+                            J[i] = '0';
+                    }
+                }
+
+                //int res1 = int.Parse(string.Join(string.Empty, C));
+                //int res2 = int.Parse(string.Join(string.Empty, J));
+                //int res3 = int.Parse(string.Join(string.Empty, C2));
+                //int res4 = int.Parse(string.Join(string.Empty, J2));
+                //int res5 = int.Parse(string.Join(string.Empty, C2));
+                //int res6 = int.Parse(string.Join(string.Empty, J2));
+
+                //if (Math.Abs(res1 - res2) < Math.Abs(res3 - res4))
+                //{
+                //    Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //        string.Join(string.Empty, C), string.Join(string.Empty, J)));
+                //}
+                //else if (Math.Abs(res1 - res2) > Math.Abs(res3 - res4))
+                //{
+                //    Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //        string.Join(string.Empty, C2), string.Join(string.Empty, J2)));
+                //}
+                //else
+                //{
+                //    if (res1 < res3)
+                //        Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //            string.Join(string.Empty, C), string.Join(string.Empty, J)));
+                //    else if (res2 < res3)
+                //        Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //            string.Join(string.Empty, C2), string.Join(string.Empty, J2)));
+                //    else if (res2 < res4)
+                //        Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //            string.Join(string.Empty, C), string.Join(string.Empty, J)));
+                //    else
+                //        Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                //            string.Join(string.Empty, C2), string.Join(string.Empty, J2)));
+                //}
+
+                Console.WriteLine(string.Format("Case #{0}: {1} {2}", nbCase,
+                    string.Join(string.Empty, C), string.Join(string.Empty, J)));
+            }
+        }
+    }
+}
