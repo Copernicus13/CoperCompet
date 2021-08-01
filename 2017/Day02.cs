23630bc24dfd3bdd5209ec8c1d7d02eeef5ccb5e
Michel <copernicus_13@hotmail.com>
2021-08-01 21:52:47 +0200

    [2017] Completed Day02

---
+++ 2017/Day02.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace AdventOfCode._2017
+{
+    /// <summary>
+    /// http://adventofcode.com/2017/day/2
+    /// </summary>
+    public class Day02
+    {
+        private Func<string, IList<int>> StrToIntList =
+            a => a.Split(' ', '\t')
+                  .Select(s => int.Parse(s))
+                  .ToList();
+
+        public Day02(Part p)
+        {
+            int result = 0;
+            string line;
+            while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+            {
+                var list = StrToIntList(line).ToList();
+                if (p == Part.Part1)
+                    result += list.Max() - list.Min();
+                else if (p == Part.Part2)
+                    result += FindAndComputeEvenlyDivisibleValues(list);
+            }
+            Console.WriteLine(result);
+        }
+
+        private int FindAndComputeEvenlyDivisibleValues(List<int> list)
+        {
+            for (int i = 0; i < list.Count; ++i)
+                for (int j = 0; j < list.Count; ++j)
+                    if (i != j && list[i] % list[j] == 0)
+                        return list[i] / list[j];
+            return -1;
+        }
+    }
+}
