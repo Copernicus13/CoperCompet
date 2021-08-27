34e002125209775b6724840800fdd0462dee03b5
Michel <copernicus_13@hotmail.com>
2021-08-28 00:32:03 +0200

    [2021] Project initialization.

---
+++ 2021/Qualification/B. Xs and Os.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerCup._2021.Qualification
+{
+    public class XsAndOs
+    {
+        public XsAndOs()
+        {
+            int T = int.Parse(Console.ReadLine());
+        }
+    }
+}
