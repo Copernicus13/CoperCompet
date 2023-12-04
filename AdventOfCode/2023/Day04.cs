030ed710dc4d3d58828fc48cbafc3f510689a683
Michel P. <copernicus_13@hotmail.com>
2023-12-04 10:21:40 +0100

    [AoC][2023] Clean-up Day04

--- AdventOfCode/2023/Day04.cs
+++ AdventOfCode/2023/Day04.cs
@@ -12,48 +12,41 @@ namespace AdventOfCode._2023
     {
         private class Game
         {
-            public int IdGame { get; set; }
             public IList<int> WinningNumbers { get; } = new List<int>();
             public IList<int> MyNumbers { get; } = new List<int>();
 
-            private bool IsWinner => WinningNumbers.Intersect(MyNumbers).Any();
             public int NbWinner => WinningNumbers.Intersect(MyNumbers).Count();
-            public int Score => IsWinner ? (int)Math.Pow(2, NbWinner - 1) : 0;
-
+            public int Score => NbWinner != 0 ? (int)Math.Pow(2, NbWinner - 1) : 0;
         }
 
         public Day04(Part p)
         {
             var result = 0;
             string line;
-            IList<(int NbWinner, int NbCard)> win = new List<(int NbWinner, int NbCard)>();
+            IList<(int NbWinner, int NbCard)> deck = new List<(int, int)>();
+
             while (!string.IsNullOrEmpty(line = Console.ReadLine()!))
             {
                 if (p == Part.Part1)
                     result += ParseLine(line).Score;
                 else if (p == Part.Part2)
-                {
-                    var game = ParseLine(line);
-                    win.Add((game.NbWinner, 1));
-                }
+                    deck.Add((ParseLine(line).NbWinner, 1));
             }
 
             if (p == Part.Part2)
             {
-                for (int i = 0; i < win.Count; ++i)
-                {
-                    for (int x = 0; x < win[i].NbCard; ++x)
-                        for (int j = i + 1; j < win.Count && j < i + 1 + win[i].NbWinner; ++j)
-                            win[j] = (win[j].NbWinner, win[j].NbCard + 1);
-                }
-                result = win.Sum(s => s.NbCard);
+                for (int i = 0; i < deck.Count; ++i)
+                    for (int j = i + 1; j < deck.Count && j < i + 1 + deck[i].NbWinner; ++j)
+                        deck[j] = (deck[j].NbWinner, deck[j].NbCard + deck[i].NbCard);
+
+                result = deck.Sum(s => s.NbCard);
             }
             Console.WriteLine(result);
         }
 
         private Game ParseLine(string line)
         {
-            var result = new Game { IdGame = int.Parse(line.Substring(4, line.IndexOf(':') - 4)) };
+            var result = new Game();
             var allNumbers = line.Split(':')[1].Split('|').Select(s => s.Trim()).ToList();
             foreach (var winningNumber in allNumbers[0].Split(' ', StringSplitOptions.RemoveEmptyEntries))
                 result.WinningNumbers.Add(int.Parse(winningNumber));
