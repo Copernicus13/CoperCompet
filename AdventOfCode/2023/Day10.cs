8e93a69c9b355f9f80931870ff9c879dd6763cb2
Michel P. <copernicus_13@hotmail.com>
2023-12-10 17:26:03 +0100

    [AoC][2023] Clean-up Day10

--- AdventOfCode/2023/Day10.cs
+++ AdventOfCode/2023/Day10.cs
@@ -12,9 +12,8 @@ namespace AdventOfCode._2023
     /// </summary>
     public class Day10
     {
-        private const int MaTabSize = 140;
-        private Point[] _Dir = { new Point(-1, 0), new Point(0, -1), new Point(1, 0), new Point(0, 1) };
-        private enum DirName { Left, Up, Right, Down, Start };
+        private readonly Point[] _dir = { new(-1, 0), new(0, -1), new(1, 0), new(0, 1) };
+        private enum DirName { Left, Up, Right, Down, Special };
 
         private struct Position
         {
@@ -23,26 +22,26 @@ namespace AdventOfCode._2023
             public int Distance;
         }
 
-        private readonly char[,] _tab;
-        private readonly char[,] _tab2;
         private static readonly char[] SourceArray = { '─', '│', '┌', '┐', '└', '┘', 'S' };
 
+        /// <remarks>Adjust line 123 to start correctly</remarks>
         public Day10(Part p)
         {
+            int result = 0;
             string line = Console.ReadLine()!;
-            _tab = new char[line.Length, line.Length];
+            char[,] tab = new char[line.Length, line.Length];
             for (var x = 0; x < line.Length; ++x)
-                _tab[0, x] = line[x];
+                tab[0, x] = line[x];
             for (int y = 1; !string.IsNullOrEmpty(line = Console.ReadLine()!); ++y)
                 for (var x = 0; x < line.Length; ++x)
-                    _tab[y, x] = line[x];
+                    tab[y, x] = line[x];
 
-            var indexStart = _tab.ToList().IndexOf('S');
+            var indexStart = tab.ToList().IndexOf('S');
             var currentPos =
                 new Position
                 {
-                    Pos = new Point(indexStart % _tab.GetLength(1), indexStart / _tab.GetLength(1)),
-                    DirName = DirName.Start,
+                    Pos = new Point(indexStart % tab.GetLength(1), indexStart / tab.GetLength(1)),
+                    DirName = DirName.Special,
                     Distance = 0
                 };
 
@@ -52,78 +51,64 @@ namespace AdventOfCode._2023
                 currentPos.DirName = NextDir(currentPos.DirName, currentChar);
                 ++currentPos.Distance;
                 currentPos.Pos = new Point(
-                    currentPos.Pos.X + _Dir[(int)currentPos.DirName].X,
-                    currentPos.Pos.Y + _Dir[(int)currentPos.DirName].Y);
-                currentChar = _tab[currentPos.Pos.Y, currentPos.Pos.X];
-                _tab[currentPos.Pos.Y, currentPos.Pos.X] = PrettyPrint(currentChar); // for part 2
+                    currentPos.Pos.X + _dir[(int)currentPos.DirName].X,
+                    currentPos.Pos.Y + _dir[(int)currentPos.DirName].Y);
+                currentChar = tab[currentPos.Pos.Y, currentPos.Pos.X];
+                tab[currentPos.Pos.Y, currentPos.Pos.X] = PrettyPrint(currentChar); // for part 2
             }
 
             if (p == Part.Part1)
-                Console.WriteLine(currentPos.Distance / 2);
+                result = currentPos.Distance / 2;
             else if (p == Part.Part2)
             {
-                _tab[indexStart / _tab.GetLength(1), indexStart % _tab.GetLength(1)] = '┐';
+                for (int y = 0; y < tab.GetLength(0); ++y)
+                    for (var x = 0; x < tab.GetLength(1); ++x)
+                        if (!SourceArray.Contains(tab[y, x]))
+                            tab[y, x] = ' ';
 
-                for (int y = 0; y < _tab.GetLength(0); ++y)
-                    for (var x = 0; x < _tab.GetLength(1); ++x)
-                        if (!SourceArray.Contains(_tab[y, x]))
-                            _tab[y, x] = ' ';
-
-                for (int i = 0; i < _tab.GetLength(0); ++i)
-                    Console.WriteLine(_tab.GetRow(i));
-
-                _tab2 = new char[_tab.GetLength(0) * 2, _tab.GetLength(1) * 2];
-
-                // agrandir la map initiale par 2
-                for (int y = 0; y < _tab.GetLength(0); ++y)
-                    for (var x = 0; x < _tab.GetLength(1); ++x)
+                // Expand map by factor 2
+                char[,] tab2 = new char[tab.GetLength(0) * 2, tab.GetLength(1) * 2];
+                for (int y = 0; y < tab.GetLength(0); ++y)
+                    for (int x = 0; x < tab.GetLength(1); ++x)
                     {
-                        _tab2[2 * y, 2 * x] = _tab[y, x];
-                        _tab2[2 * y, 2 * x + 1] = AllongeNextChar(DirName.Right, _tab[y, x]);
-                        _tab2[2 * y + 1, 2 * x] = AllongeNextChar(DirName.Down, _tab[y, x]);
-                        _tab2[2 * y + 1, 2 * x + 1] = ' ';
+                        tab2[2 * y, 2 * x] = tab[y, x];
+                        tab2[2 * y, 2 * x + 1] = ExtendChar(DirName.Right, tab[y, x]);
+                        tab2[2 * y + 1, 2 * x] = ExtendChar(DirName.Down, tab[y, x]);
+                        tab2[2 * y + 1, 2 * x + 1] = ExtendChar(DirName.Special, tab[y, x]);
                     }
 
-                //for (int i = 0; i < 100; ++i)
-                //    Console.WriteLine(_tab2.GetRow(i).Take(180).ToArray());
+                Fill(tab2, 0, 0);
+                Fill(tab2, 0, tab2.GetLength(0) - 1);
+                Fill(tab2, tab2.GetLength(1) - 1, 0);
+                Fill(tab2, tab2.GetLength(1) - 1, tab2.GetLength(0) - 1);
 
-                Fill(0, 0);
-                Fill(_tab2.GetLength(1) - 1, 0);
-                Fill(_tab2.GetLength(1) - 1, _tab2.GetLength(0) - 1);
-                Fill(0, _tab2.GetLength(0) - 1);
+                // Shrink map by 2
+                for (int y = 0; y < tab.GetLength(0); ++y)
+                    for (var x = 0; x < tab.GetLength(1); ++x)
+                        tab[y, x] = tab2[y * 2, x * 2];
 
-                // rétrécir la map agrandie par 2
-                for (int y = 0; y < _tab.GetLength(0); ++y)
-                    for (var x = 0; x < _tab.GetLength(1); ++x)
-                        _tab[y, x] = _tab2[y * 2, x * 2];
-
-                for (int i = 0; i < _tab.GetLength(0); ++i)
-                    Console.WriteLine(_tab.GetRow(i));
-
-                //for (int i = 0; i < _tab2.GetLength(0); ++i)
-                //    Console.WriteLine(_tab2.GetRow(i).Take(230).ToArray());
-
-                Console.WriteLine(_tab.ToList().Count(c => c == ' '));
+                result = tab.ToList().Count(c => c == ' ');
             }
+            Console.WriteLine(result);
         }
 
-        private void Fill(int x, int y)
+        private void Fill(char[,] tab, int x, int y)
         {
             var queue = new Queue<Point>();
             queue.Enqueue(new Point(x, y));
             while (queue.Count > 0)
             {
                 var p = queue.Dequeue();
-                if (_tab2[p.Y, p.X] != ' ')
+                if (tab[p.Y, p.X] != ' ')
                     continue;
-                _tab2[p.Y, p.X] = 'x';
-                foreach (var dir in _Dir)
+                tab[p.Y, p.X] = 'x';
+                foreach (var dir in _dir)
                 {
                     var curX = p.X + dir.X;
                     var curY = p.Y + dir.Y;
-                    if (curX >= 0 && curX < _tab2.GetLength(1) &&
-                        curY >= 0 && curY < _tab2.GetLength(0) &&
-                        _tab2[curY, curX] == ' ')
+                    if (curX >= 0 && curX < tab.GetLength(1) &&
+                        curY >= 0 && curY < tab.GetLength(0) &&
+                        tab[curY, curX] == ' ')
                     {
                         queue.Enqueue(new Point(curX, curY));
                     }
@@ -134,8 +119,8 @@ namespace AdventOfCode._2023
         private static DirName NextDir(DirName currentDir, char currentChar) =>
             (currentDir, currentChar) switch
             {
-                // Start is manually adjusted for the input
-                (DirName.Start, char.MinValue) => DirName.Down,
+                // Start is manually adjusted for my input
+                (DirName.Special, char.MinValue) => DirName.Down,
 
                 (DirName.Left, '-') => DirName.Left,
                 (DirName.Left, 'L') => DirName.Up,
@@ -152,12 +137,15 @@ namespace AdventOfCode._2023
                 _ => throw new Exception("Invalid input")
             };
 
-        private static char AllongeNextChar(DirName currentDir, char currentChar) =>
+        private static char ExtendChar(DirName currentDir, char currentChar) =>
             (currentDir, currentChar) switch
             {
+                (DirName.Special, 'S') => 'S',
+                (DirName.Right, 'S') => 'S',
                 (DirName.Right, '─') => '─',
                 (DirName.Right, '┌') => '─',
                 (DirName.Right, '└') => '─',
+                (DirName.Down, 'S') => 'S',
                 (DirName.Down, '┌') => '│',
                 (DirName.Down, '│') => '│',
                 (DirName.Down, '┐') => '│',
