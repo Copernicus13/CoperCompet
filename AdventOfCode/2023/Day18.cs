e9ea770a2f014ce3801ea861288aff429cffa847
Michel P. <copernicus_13@hotmail.com>
2023-12-21 22:47:48 +0100

    [AoC][2023] Clean-up Day18

--- AdventOfCode/2023/Day18.cs
+++ AdventOfCode/2023/Day18.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using AdventOfCode.Common;
 using CoperAlgoLib.Data;
+using CoperAlgoLib.Geometry;
 
 namespace AdventOfCode._2023
 {
@@ -16,28 +16,15 @@ namespace AdventOfCode._2023
         private const int MapWidth = 1000;
         private const int MapHeight = 1000;
 
-        private readonly Point[] _dirTab = { new(1, 0), new(0, 1), new(-1, 0), new(0, -1) };
+        // Ordered specifically for the problem
+        private readonly PointL[] _dirTab = { new(1, 0), new(0, 1), new(-1, 0), new(0, -1) };
         public enum DirName { R, D, L, U };
 
-        public struct Point
-        {
-            public long X;
-            public long Y;
-
-            public Point(long x, long y)
-            {
-                X = x;
-                Y = y;
-            }
-        }
-
-        public record Data(char Dir, int nbMeter, Color Color);
-
-        /// <remarks>For part1, adjust line 72 to fill correctly</remarks>
+        /// <remarks>For part1, adjust line 57 to fill correctly</remarks>
         public Day18(Part p)
         {
+            long result = 0;
             string line;
-            var map = new List<List<char>>();
             var instr = new List<string>();
 
             while (!string.IsNullOrEmpty(line = Console.ReadLine()!))
@@ -50,8 +37,8 @@ namespace AdventOfCode._2023
 
             if (p == Part.Part1)
             {
-                var pt = new Point(MapWidth >> 1, MapHeight >> 1);
-                long minX = MapWidth, maxX = 0, minY = MapHeight, maxY = 0;
+                var pt = new PointL(MapWidth >> 1, MapHeight >> 1);
+                long minX = MapWidth, minY = MapHeight;
                 foreach (string ins in instr)
                 {
                     var dir = (DirName)Enum.Parse(typeof(DirName), ins.Split(' ')[0]);
@@ -63,40 +50,39 @@ namespace AdventOfCode._2023
                         pt.Y += _dirTab[(int)dir].Y;
                         tab[pt.Y, pt.X] = '#';
                         minX = Math.Min(minX, pt.X);
-                        maxX = Math.Max(maxX, pt.X);
                         minY = Math.Min(minY, pt.Y);
-                        maxY = Math.Max(maxY, pt.Y);
                     }
                 }
                 // Filling coordinate is manually adjusted for my input
                 Fill(tab, minX + 334, minY + 1);
-                Console.WriteLine(tab.ToList().Count(c => c == '#'));
+                result = tab.ToList().Count(c => c == '#');
             }
-            else
+            else if (p == Part.Part2)
             {
-                var pt = new Point(0, 0);
+                var startPoint = new PointL(0, 0);
                 long perimeter = 0;
-                var d = new List<Point> { pt };
+                var vertices = new List<PointL> { startPoint };
                 foreach (string ins in instr)
                 {
-                    var j = ins.Split('#')[1].TrimEnd(')');
-                    var dir = (DirName)Enum.Parse(typeof(DirName), j[5..]);
-                    int length = int.Parse(j[..5], NumberStyles.HexNumber);
+                    string code = ins.Split('#')[1].TrimEnd(')');
+                    var dir = (DirName)Enum.Parse(typeof(DirName), code[5..]);
+                    int length = int.Parse(code[..5], NumberStyles.HexNumber);
                     perimeter += length;
-                    d.Add(
-                        new Point(d.Last().X + _dirTab[(int)dir].X * length,
-                            d.Last().Y + _dirTab[(int)dir].Y * length));
+                    vertices.Add(
+                        new PointL(vertices.Last().X + _dirTab[(int)dir].X * length,
+                            vertices.Last().Y + _dirTab[(int)dir].Y * length));
                 }
-                Console.WriteLine(ShoeLace(d) + perimeter / 2 + 1);
+                result = ShoeLace(vertices) + perimeter / 2 + 1;
             }
+            Console.WriteLine(result);
         }
 
-        private long ShoeLace(List<Point> points)
+        private static long ShoeLace(List<PointL> points)
         {
             checked
             {
                 long result = 0;
-                for (var i = 1; i < points.Count; ++i)
+                for (int i = 1; i < points.Count; ++i)
                     result += points[i - 1].X * points[i].Y - points[i].X * points[i - 1].Y;
                 result += points.Last().X * points.First().Y - points.First().X * points.Last().Y;
                 return result / 2;
@@ -105,8 +91,8 @@ namespace AdventOfCode._2023
 
         private void Fill(char[,] tab, long x, long y)
         {
-            var queue = new Queue<Point>();
-            queue.Enqueue(new Point(x, y));
+            var queue = new Queue<PointL>();
+            queue.Enqueue(new PointL(x, y));
             while (queue.Count > 0)
             {
                 var p = queue.Dequeue();
@@ -115,13 +101,13 @@ namespace AdventOfCode._2023
                 tab[p.Y, p.X] = '#';
                 foreach (var dir in _dirTab)
                 {
-                    var curX = p.X + dir.X;
-                    var curY = p.Y + dir.Y;
+                    long curX = p.X + dir.X;
+                    long curY = p.Y + dir.Y;
                     if (curX >= 0 && curX < tab.GetLength(1) &&
                         curY >= 0 && curY < tab.GetLength(0) &&
                         tab[curY, curX] == '.')
                     {
-                        queue.Enqueue(new Point(curX, curY));
+                        queue.Enqueue(new PointL(curX, curY));
                     }
                 }
             }
